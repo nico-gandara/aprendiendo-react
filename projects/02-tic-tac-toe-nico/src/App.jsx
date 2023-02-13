74ff35fc5651f0446afcfe9fd2a86946c3d4436c
Nicolás Gándara <ngandara@summasolutions.net>
2023-02-13 16:13:52 -0300

    Eslint installation + codign standard app.jsx

--- projects/02-tic-tac-toe-nico/src/App.jsx
+++ projects/02-tic-tac-toe-nico/src/App.jsx
@@ -1,77 +1,84 @@
-import { useState } from "react"
-import confetti from "canvas-confetti"
-import { Square } from "./components/Square"
-import { TURNS, WINNER_COMBOS } from "./constants"
-import { checkWinnerFrom, checkEndGame } from "./logic/board"
-import { WinnerModal } from "./components/WinnerModal"
+import { useState } from 'react'
+import confetti from 'canvas-confetti'
+import { Square } from './components/Square'
+import { TURNS } from './constants'
+import { checkWinnerFrom, checkEndGame } from './logic/board'
+import { WinnerModal } from './components/WinnerModal'
 
-function App() {
-    const [board, setBoard] = useState(Array(9).fill(null))
+function App () {
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    if (boardFromStorage) return JSON.parse(boardFromStorage)
+    return Array(9).fill(null)
+  })
 
-    const [turn, setTurn] = useState(TURNS.X)
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ? JSON.parse(turnFromStorage) : TURNS.X
+  })
 
-    const [winner, setWinner] = useState(null)
+  const [winner, setWinner] = useState(null)
 
-    
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
+  }
 
-    const resetGame = () => {
-        setBoard(Array(9).fill(null))
-        setTurn(TURNS.X)
-        setWinner(null)
-    }
+  const updateBoard = (index) => {
+    if (board[index] || winner) return
 
-    const updateBoard = (index) => {
+    // actualizo tablero
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
 
-        if (board[index] || winner) return
-        
-        //actualizo tablero
-        const newBoard =[...board]
-        newBoard[index]= turn
-        setBoard(newBoard)
+    // actualizo turno
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
 
-        //actualizo turno
-        const newTurn = turn == TURNS.X ? TURNS.O : TURNS.X
-        setTurn(newTurn)
+    window.localStorage.setItem('board', JSON.stringify(newBoard))
+    window.localStorage.setItem('turn', JSON.stringify(newTurn))
 
-        const newWinner = checkWinnerFrom(newBoard)
-        if (newWinner) {
-            confetti()
-            setWinner(newWinner)
-        } else if (checkEndGame(newBoard)) 
-            setWinner(false)
-    }
-    
-    return (
+    const newWinner = checkWinnerFrom(newBoard)
+    if (newWinner) {
+      confetti()
+      setWinner(newWinner)
+    } else if (checkEndGame(newBoard)) { setWinner(false) }
+  }
+
+  return (
     <main className='board'>
-        <h1>Tic tac toe</h1>
-        <button onClick={resetGame}>Empezar de nuevo</button>
-        <section className='game'>
-            {
+      <h1>Tic tac toe</h1>
+      <button onClick={resetGame}>Empezar de nuevo</button>
+      <section className='game'>
+        {
                 board.map((_, index) => {
-                    return (
-                        <Square
-                          key={index}
-                          index={index}
-                          updateBoard={updateBoard}
-                        >
-                            {board[index]}
-                        </Square>
-                    )
+                  return (
+                    <Square
+                      key={index}
+                      index={index}
+                      updateBoard={updateBoard}
+                    >
+                      {board[index]}
+                    </Square>
+                  )
                 })
             }
-        </section>
-        <section className="turn">
-            <Square isSelected={turn==TURNS.X}>
-                {TURNS.X}
-            </Square>
-            <Square isSelected={turn==TURNS.O}>
-                {TURNS.O}
-            </Square>
-        </section>
-        
-        <WinnerModal resetGame={resetGame} winner={winner} />
+      </section>
+      <section className='turn'>
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
+      </section>
+
+      <WinnerModal resetGame={resetGame} winner={winner} />
     </main>
- )
+  )
 }
 export default App
-
